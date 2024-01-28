2040adad79447f20b780bd4fac3aae9d695c1805
Parisa <134794618+parisa-0@users.noreply.github.com>
2024-01-28 10:43:56 +0000

    Create Day20:Sorting.java

---
+++ Day20:Sorting.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+  /*      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+*/
+
+       Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int numberOfSwaps = 0;
+        // Write your code here
+        for(int i = n - 1; i > 0; i--) {
+            for(int j = 0; j < n - 1; j++) {
+                if(a[j] > a[j+1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    numberOfSwaps++;                
+                }
+            }
+            if (numberOfSwaps == 0) {
+            break;
+            }
+        }
+        
+        System.out.println("Array is sorted in " + numberOfSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n-1]);
+
+      //  bufferedReader.close();
+    }
+}
